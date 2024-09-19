f4040ba4200f16c414aae06e8731bfb4167fd943
Débora_Cristina <133518001+debCristina@users.noreply.github.com>
2024-09-19 11:05:12 -0300

    Add files via upload

---
+++ src/listaHeranca/Moto.java
@@ -0,0 +1,20 @@
+package _19_09_2024.exercicio;
+
+public class Moto extends Veiculo{
+    private boolean temCarenagem;
+
+    public Moto(String marca, String modelo, int ano, boolean temCarenagem) {
+        super(marca, modelo, ano);
+        this.temCarenagem = temCarenagem;
+    }
+
+    public void empinar() {
+        System.out.println("Moto empinando...");
+    }
+
+    @Override
+    public void exibirInformacoes() {
+        super.exibirInformacoes();
+        System.out.println("Tem carenagem: " +temCarenagem);
+    }
+}
