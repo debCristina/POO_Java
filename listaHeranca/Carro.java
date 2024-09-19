2e2c7c1f300816dc9005ffcd1ca2828265aad1a2
Débora_Cristina <133518001+debCristina@users.noreply.github.com>
2024-09-19 10:58:48 -0300

    Add files via upload

---
+++ listaHeranca/Carro.java
@@ -0,0 +1,20 @@
+package _19_09_2024.exercicio;
+
+public class Carro extends Veiculo{
+    private int portas;
+
+    public void abrirPortaMalas() {
+        System.out.println("Abrindo porta malas");
+    }
+
+    public Carro(String marca, String modelo, int ano, int portas) {
+        super(marca, modelo, ano);
+        this.portas = portas;
+    }
+
+    @Override
+    public void exibirInformacoes() {
+        super.exibirInformacoes();
+        System.out.println("Portas: " +portas);
+    }
+}
